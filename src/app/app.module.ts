257f559118ed693501aab959400fbafe9df3b62f
daikiojm <daiking.ca2@gmail.com>
2017-10-02 23:12:26 +0900

    最新版のAngular Materialを導入

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MdButtonModule, MdCheckboxModule, MatSlideToggleModule, MatMenuModule } from '@angular/material';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +12,18 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    MatSlideToggleModule,
+    MatMenuModule
+  ],
+  exports: [
+    MdButtonModule,
+    MdCheckboxModule,
+    MatSlideToggleModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
